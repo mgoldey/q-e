303ba698292b0cff42166c41d4dd5afbcfa45392
nbrawand <nicholasbrawand@gmail.com>
2016-02-15 20:44:59 -0600

    updated coupling python debug scpt

--- scripts/coupling_debuger.py
+++ scripts/coupling_debuger.py
@@ -50,19 +50,78 @@ wmatdown = np.loadtxt('W2')
 #
 
 # up
-pysmat = [[round(np.linalg.det(saaup), 8), round(np.linalg.det(sabup), 8)],
-          [round(np.linalg.det(sbaup), 8), round(np.linalg.det(sbbup), 8)]]
+pysmatup = [[round(np.linalg.det(saaup), 8), round(np.linalg.det(sabup), 8)],
+            [round(np.linalg.det(sbaup), 8), round(np.linalg.det(sbbup), 8)]]
 print """== S up python vs QE ==
 {}\t{}
 {}\t{}
-""".format(pysmat[0], detsup[0], pysmat[1], detsup[1])
+""".format(pysmatup[0], detsup[0], pysmatup[1], detsup[1])
 
 # down
-pysmat = [[round(np.linalg.det(saadown), 8), round(np.linalg.det(sabdown), 8)],
-          [round(np.linalg.det(sbadown), 8), round(np.linalg.det(sbbdown), 8)]]
-print """== S up python vs QE ==
+pysmatdown = [[round(np.linalg.det(saadown), 8),
+               round(np.linalg.det(sabdown), 8)],
+              [round(np.linalg.det(sbadown), 8),
+               round(np.linalg.det(sbbdown), 8)]]
+print """== S down python vs QE ==
 {}\t{}
 {}\t{}
-""".format(pysmat[0], detsdown[0], pysmat[1], detsdown[1])
+""".format(pysmatdown[0], detsdown[0], pysmatdown[1], detsdown[1])
+#
+# compute cofac
+#
+pycup = [[np.transpose(np.linalg.inv(saaup)*pysmatup[0][0]),
+          np.transpose(np.linalg.inv(sabup)*pysmatup[0][1])],
+         [np.transpose(np.linalg.inv(sbaup)*pysmatup[1][0]),
+          np.transpose(np.linalg.inv(sbbup)*pysmatup[1][1])]]
 
-print sbaup[0, 1]
+pycdown = [[np.transpose(np.linalg.inv(saadown)*pysmatdown[0][0]),
+            np.transpose(np.linalg.inv(sabdown)*pysmatdown[0][1])],
+           [np.transpose(np.linalg.inv(sbadown)*pysmatdown[1][0]),
+            np.transpose(np.linalg.inv(sbbdown)*pysmatdown[1][1])]]
+#
+# compute Wmat
+#
+pywaaup = 0.0
+pywabup = 0.0
+pywbaup = 0.0
+pywbbup = 0.0
+for i in range(0, len(waaup)):
+    for j in range(0, len(waaup)):
+        pywaaup = pywaaup + waaup[i][j]*pycup[0][0][i][j]
+        pywabup = pywabup + wabup[i][j]*pycup[0][1][i][j]
+        pywbaup = pywbaup + wbaup[i][j]*pycup[1][0][i][j]
+        pywbbup = pywbbup + wbbup[i][j]*pycup[1][1][i][j]
+print """== W up python vs QE ==
+[{}, {}]\t[{}, {}]
+[{}, {}]\t[{}, {}]
+""".format(round(pywaaup, 8),
+           round(pywabup, 8),
+           round(wmatup[0][0], 8),
+           round(wmatup[0][1], 8),
+           round(pywbaup, 8),
+           round(pywbbup, 8),
+           round(wmatup[1][0], 8),
+           round(wmatup[1][1], 8))
+#
+#
+pywaadown = 0.0
+pywabdown = 0.0
+pywbadown = 0.0
+pywbbdown = 0.0
+for i in range(0, len(waadown)):
+    for j in range(0, len(waadown)):
+        pywaadown = pywaadown + waadown[i][j]*pycdown[0][0][i][j]
+        pywabdown = pywabdown + wabdown[i][j]*pycdown[0][1][i][j]
+        pywbadown = pywbadown + wbadown[i][j]*pycdown[1][0][i][j]
+        pywbbdown = pywbbdown + wbbdown[i][j]*pycdown[1][1][i][j]
+print """== W down python vs QE ==
+[{}, {}]\t[{}, {}]
+[{}, {}]\t[{}, {}]
+""".format(round(pywaadown, 8),
+           round(pywabdown, 8),
+           round(wmatdown[0][0], 8),
+           round(wmatdown[0][1], 8),
+           round(pywbadown, 8),
+           round(pywbbdown, 8),
+           round(wmatdown[1][0], 8),
+           round(wmatdown[1][1], 8))
